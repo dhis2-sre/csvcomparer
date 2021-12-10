ffb9053fbb7924ee3ab078c7eeafaccca55fcf74
radnov <radnov@pm.me>
2021-12-10 17:49:56 +0200

    refactor: Remove styling from Comparer and improve readability [DEVOPS-30]

--- csvcomparer/compare.py
+++ csvcomparer/compare.py
@@ -4,17 +4,33 @@ import pandas as pd
 
 
 class Comparer:
-    percentage_format = '{:+.2f}%'
-
     def __init__(self, threshold, current, previous):
         self.threshold = threshold
         self.current = current
         self.previous = previous
         self.tables = []
+        self.file_prefix = None
         self.aggregated_results = None
         self.aggregated_diff = pd.Series(dtype=float)
 
         pd.set_option('precision', 2)
+
+    def _build_comparison_tables(self, current, previous, column_name):
+        self.file_prefix = os.path.basename(previous).split('_')[0].capitalize()
+
+        merged_df = pd.merge(
+            current,
+            pd.read_csv(previous),
+            on=['Type', 'Name'],
+            how='outer',
+            suffixes=('_current', f'_{self.file_prefix}')
+        )
+
+        self.aggregated_results.insert(
+            len(self.aggregated_results.columns),
+            self.file_prefix,
+            merged_df[f'{column_name}_{self.file_prefix}']
+        )
 
     def compare(self, column_name):
         current_df = pd.read_csv(self.current)
@@ -24,37 +40,14 @@ class Comparer:
         )
 
         for report in self.previous:
-            previous_df = pd.read_csv(report)
-            file_prefix = os.path.basename(report).split('_')[0].capitalize()
+            self._build_comparison_tables(current_df, report, column_name)
 
-            merged_df = pd.merge(
-                current_df,
-                previous_df,
-                on=['Type', 'Name'],
-                how='outer',
-                suffixes=('_current', f'_{file_prefix}')
-            )
-
-            self.aggregated_results.insert(
-                len(self.aggregated_results.columns),
-                file_prefix,
-                merged_df[f'{column_name}_{file_prefix}']
-            )
-
-            diff = ((self.aggregated_results['Current'] / self.aggregated_results[file_prefix]) * 100) - 100
+            diff = ((self.aggregated_results['Current'] / self.aggregated_results[self.file_prefix]) * 100) - 100
             self.aggregated_diff = self.aggregated_diff.append(diff)
-            self.aggregated_results.insert(len(self.aggregated_results.columns), f'{file_prefix} Diff', diff)
+            self.aggregated_results.insert(len(self.aggregated_results.columns), f'{self.file_prefix} Diff', diff)
 
-        diff_columns = [col for col in self.aggregated_results.columns.values if 'Diff' in col]
+        self.tables.append(dict(title=column_name, body=self.aggregated_results))
 
-        columns_format = {col: self.percentage_format for col in diff_columns}
+        logging.info(f'Comparison for {column_name} column:\n {self.aggregated_results.to_string()}\n\n')
 
-        styled_results = self.aggregated_results.style.format(columns_format, na_rep='NaN')
-        styled_results.applymap(lambda x: 'color: red' if (x > self.threshold) else None, subset=diff_columns)
-
-        self.tables.append(dict(title=column_name, body=styled_results.render()))
-
-        comparison_table_string = self.aggregated_results.to_string()
-        logging.info(f'Comparison for {column_name} column:\n {comparison_table_string}\n\n')
-
-        return self.aggregated_diff.add_prefix(f'({column_name})_')
+        # return self.aggregated_diff.add_prefix(f'({column_name})_')
