b9be6c2d43583eb3b11b9e6cdc2d7fa09248632c
radnov <radnov@pm.me>
2021-12-10 15:08:12 +0200

    refactor: Rename comparison to comparer [DEVOPS-30]

--- csvcomparer/report.py
+++ csvcomparer/report.py
@@ -2,13 +2,13 @@ from jinja2 import Environment, FileSystemLoader
 
 
 class Reporter:
-    def __init__(self, comparison, output_file):
-        self.comparison = comparison
+    def __init__(self, comparer, output_file):
+        self.comparer = comparer
         self.output_file = output_file
 
     def render(self):
-        template = Environment(loader=FileSystemLoader('data')).get_template("comparison-template.html")
-        html = template.render(tables=self.comparison.tables)
+        template = Environment(loader=FileSystemLoader('templates')).get_template("comparison-template.html")
+        html = template.render(tables=self.comparer.tables)
         html_file = open(self.output_file, "w")
         html_file.write(html)
         html_file.close()
