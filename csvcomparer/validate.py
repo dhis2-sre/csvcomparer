f03c69b6b7969c7367c19e558a7f569da1fe3000
radnov <radnov@pm.me>
2021-12-10 15:08:56 +0200

    refactor: Wrap validations in descriptive methods [DEVOPS-30]

--- csvcomparer/validate.py
+++ csvcomparer/validate.py
@@ -1,20 +1,32 @@
+import logging
 import sys
 import pandas as pd
 
 
 class Validator:
-    def __init__(self, comparison, results):
-        self.comparison = comparison
+    def __init__(self, comparer, results):
+        self.comparer = comparer
         self.results = results
 
+    def _filter_null_results(self):
+        return list(filter(pd.notnull, self.results))
+
+    def _all_results_below_threshold(self, results):
+        return all(result <= self.comparer.threshold for result in results)
+
+    def _any_result_above_threshold(self, results):
+        return any(result > self.comparer.threshold for result in results)
+
     def validate(self):
-        print(f'Allowed threshold: {self.comparison.threshold}\n')
+        logging.info(f'Allowed threshold: {self.comparer.threshold}\n')
 
-        print(self.results)
+        logging.info(self.results)
 
-        if all(result <= self.comparison.threshold for result in self.results.array if pd.notnull(result)):
+        filtered_results = self._filter_null_results()
+
+        if self._all_results_below_threshold(filtered_results):
             sys.exit()
-        elif any(result > self.comparison.threshold for result in self.results.array if pd.notnull(result)):
+        elif self._any_result_above_threshold(filtered_results):
             sys.exit('Some of the requests are above the given threshold factor!')
         else:
             sys.exit('An error occurred!')
